4497153e6ed3c5ebf1d71650ef0495904ef779c0
Matheus Marques <matheusmarques03@yahoo.com>
2022-02-09 01:44:02 -0300

    feat: implement transfer rule

--- app/services/transference.rb
+++ app/services/transference.rb
@@ -35,10 +35,25 @@ class Transference
     end
   
     def new_balance_sender
-      @sender.balance - @value
+      @sender.balance - (@value + transfer_fee_calculation + transference_over_thousand)
     end
   
     def new_balance_recipient
       @recipient.balance + @value
+    end
+
+    def transference_over_thousand
+      @value > 1000 ? 10 : 0
+    end
+    
+    def its_weekend?
+      time = Time.now.strftime("%H%M")
+      day = Time.now.strftime("%u")
+  
+      day.between?(6, 7) || !time.between?("0900", "1800")
+    end
+
+    def transfer_fee_calculation
+      its_weekend? ? 7 : 5
     end
   end
